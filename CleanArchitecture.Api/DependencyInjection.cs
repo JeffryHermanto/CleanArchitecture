fa1cba37af1f4fb66f6112008f8a97f262d2093a
Jeffry Hermanto <jeffryhermanto@Jeffrys-MacBook-Pro.local>
2026-03-23 18:51:46 +0700

    Configure proper project references and centralize dependency injection using best practices

---
+++ CleanArchitecture.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infrastructure;
+
+namespace CleanArchitecture.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAppDI(this IServiceCollection services)
+    {
+        services
+            .AddApplicationDI()
+            .AddInfrastructureDI();
+
+        return services;
+    }
+}
---
+++ CleanArchitecture.Domain/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomainDI(this IServiceCollection services)
+    {
+        return services;
+    }
+}
