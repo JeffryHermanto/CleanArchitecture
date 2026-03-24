6ff5b633e11b3a0a15db9ecb29e0b9e75da1a9bc
Jeffry Hermanto <jeffryhermanto@Jeffrys-MacBook-Pro.local>
2026-03-24 11:36:29 +0700

    Implement Scalar API Documentation UI

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -1,26 +1,27 @@
 using CleanArchitecture.Api;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddAppDI(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddAppDI(builder.Configuration);
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference(options =>
+        {
+            options.Title = "Clean Architecture API Docs";
+            options.Theme = ScalarTheme.Default;
+        });
+    }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
